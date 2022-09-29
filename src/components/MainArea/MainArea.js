01b8aac6fc3fda88939d2a2bc9df9749ebf1a352
anowarzz <anowarhosen444@gmail.com>
2022-09-29 09:09:35 +0600

    card information added

--- src/components/MainArea/MainArea.js
+++ src/components/MainArea/MainArea.js
@@ -3,23 +3,35 @@
 
 const MainArea = ({myActivity}) => {
 
-    const {id, activityName, time, image} = myActivity;
+    const {id, activityName, time, description, image} = myActivity;
 
-    console.log(id, activityName, time, image);
+    console.log(id, activityName, time, description, image);
     
     
 
     return (
-       <div className="grid">
+       <div>
         
-        <div className="bg-white">
+        {/* <div className="">
             
         <img src={image} alt="" className="w-64"/>
         <h3 className="text-2xl font-bold">{activityName}</h3>
-        <p>Time Required: {time}</p>
+        <p className="font-semibold">Time Required: {time} Minutes</p>
+        <p>{description}</p>
         <button className="btn btn-secondary">Button</button>
-
-        </div>
+        
+        </div> */}
+        <div className="card bg-base-100 shadow-xl mb-3 mx-2">
+  <img className="p-3 rounded-2xl mb-0" src={image} alt="p" />
+  <div className="card-body">
+    <h2 className="card-title mt-0">{activityName}</h2>
+    <p>{description}</p>
+    <p className="font-semibold">Time Required: {time} Minutes</p>
+    <div className="card-actions justify-center ">
+      <button className="btn btn-primary w-52 mt-4">Add To List</button>
+    </div>
+  </div>
+</div>
 
        </div>
     );
--- src/components/MyActivity/MyActivity.js
+++ src/components/MyActivity/MyActivity.js
@@ -25,9 +25,11 @@ const MyActivity = () => {
    <div className='col-span-5'>
    <Navbar> </Navbar>
 
+   <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 pt-4 gap-2 bg-base-200'>
    {
     activities.map(activity => <MainArea myActivity ={activity} key={activity.id}> </MainArea>)
    }
+   </div>
    
     </div>
 
